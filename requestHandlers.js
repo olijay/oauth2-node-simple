f4a3a029fc073242ece839f6c4d7069aeb3c39f9
Olafur Jens Olafsson <olijay@gmail.com>
2013-11-14 20:24:11 +0100

    Two instances of the node server need to run, one at some port where the client accesses it (the auth server) and the other at port 8889 (the resource server)

--- requestHandlers.js
+++ requestHandlers.js
@@ -2,6 +2,9 @@
 var exec = require("child_process").exec;
 var url = require("url");
 var path = require("path");
+var http = require('http');
+var querystring = require('querystring');
+var token_array = [];
 
 function index(request, response) {
     fs.readFile("./index.html", function (err, data) {
@@ -23,18 +26,28 @@ function index(request, response) {
     });
 }
 
-function authorizeImplicit(request, response, postData) {
-    console.log("authorizeImplicit hit!");
-    var authorizeImplicitRequest = querystring.parse(postData);
+function authorizeImplicit(request, response) {
+    console.log("authorizeImplicit hit! request.url: " + request.url);
+
+    //var authorizeImplicitRequest = querystring.parse(postData);
+    
+
+    var authorizeImplicitRequest = url.parse(request.url, true).query;
+    //console.log("authorizeImplicitRequest: " + authorizeImplicitRequest.response_type 
+    //    + " " + authorizeImplicitRequest.client_id 
+    //    + " " + authorizeImplicitRequest.redirect_uri);
     // verify that we have a valid request
-    if (authorizeImplicitRequest.response_type === "token" &&
+    // skip all of this and end the response if response_type and client_id were incorrect
+    if (authorizeImplicitRequest.response_type == "token" &&
         authorizeImplicitRequest.client_id) {
 
         if (authorizeImplicitRequest.redirect_uri) {
             if (authorizeImplicitRequest.redirect_uri != "http://localhost:8889") {
+                console.log("redirect_uri was incorrect");
                 response.end(); // redirect_uri was incorrect
             }
         }
+        console.log("user is " + authorizeImplicitRequest.client_id);
         // what follows is the authorization logic (only two client_id's are valid)
         if (authorizeImplicitRequest.client_id === "olafurj") {
             // TODO generate a token
@@ -45,25 +58,83 @@ function authorizeImplicit(request, response, postData) {
             var access_token = "qwerty";
 
         }
-        // TODO register token with 8889
 
+        console.log("access_token is:" + access_token);
+        // register token with 8889
+         // Build the post string from an object
+         console.log("about to register token. client_id: " + authorizeImplicitRequest.client_id + " access_token: " + access_token);
+          var post_data = querystring.stringify({
+              'client_id' : authorizeImplicitRequest.client_id,
+              'access_token': access_token
+          });
+
+          // An object of options to indicate where to post to
+          var post_options = {
+              host: 'localhost',
+              port: '8889',
+              path: '/registerToken',
+              method: 'POST',
+              headers: {
+                  'Content-Type': 'application/x-www-form-urlencoded',
+                  'Content-Length': post_data.length
+              }
+          };
+
+          // Set up the request
+          var registerToken_request = http.request(post_options, function(res) {
+              res.setEncoding('utf8');
+              res.on('data', function (chunk) {
+                  console.log('Response: ' + chunk);
+              });
+          });
+
+        registerToken_request.on('response', function(res) {
+            console.log('status code: ' + res.statusCode);            
+        });
+
+          // register the token
+          registerToken_request.write(post_data);
+          registerToken_request.end();
+
+          // respond to the auth request
         response.writeHead(302, { 'Content-Type': 'application/x-www-form-urlencoded', 
-                'Location' : 'http://localhost:8889/requestResource#access_token=' + access_token + 'token_type=bearer&expires_in=3600' });
-
-    }
-    else { // response_type and client_id were incorrect
-        
-    }
+                'Location' : 'http://localhost:8889/requestResource?access_token=' + access_token + '&token_type=bearer&expires_in=3600' });
+    
+    }    
     response.end()
     
 }
-
+// auth server running on 8888 calles this function on the resource server running on port 8889
 function registerToken(request,response,postData) {
-    // TODO add token to array with a lifetime of 3600 seconds
+    // add token to array with a lifetime of an hour
+    var token_data = querystring.parse(postData);
+    console.log("registerToken hit!");
+    console.log("token_data.access_token: " + token_data.access_token + " client_id: " +token_data.client_id);
+    token_data.expires_on = new Date() + 3600*1000; // one hour from now in millisecs
+    token_array.push(token_data);
+    response.writeHead(200, { 'Content-Type': 'text/plain' });
+    response.end();
+
 }
 
-function requestResource(request, response, postData) {
-    // TODO return the given clientID's full name
+// the client calles this on the resource server running on port 8889
+function requestResource(request, response) {
+    // return the given clientID's full name
+    //var request_data = querystring.parse(postData);
+    console.log("requestResource hit!");
+    console.log("request.url: " + request.url);
+
+
+
+    var response_data = JSON.stringify({
+             'fullname' : 'Gert L Mikkelsen',
+             'client_id' : 'gertl'
+         });
+    console.log("requestResource response_data: " + response_data);
+    response.write(response_data);
+    response.writeHead(200, { 'Content-Type': 'application/json', 'Content-Length' :response_data.length});
+    response.end();
+
 }
 
 exports.index = index;
